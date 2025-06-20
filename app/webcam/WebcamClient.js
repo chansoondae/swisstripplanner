cea016ce16ab3f17fa2012325c845bbe05819775
chansoondae <info@mongtravel.net>
2025-06-20 14:09:46 +0900

    unoptimized for webcam

--- app/webcam/WebcamClient.js
+++ app/webcam/WebcamClient.js
@@ -60,6 +60,7 @@ export default function WebcamClient({ webcams }) {
                     sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                     className="object-cover"
                     priority={false}
+                    unoptimized
                   />
                 </div>
                 
